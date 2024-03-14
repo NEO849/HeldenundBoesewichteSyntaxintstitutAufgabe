3d8733872de4c6af9af9c0bdf7f82ee766d5e16f
Michael Fleps <f.michi84.989@gmail.com>
2024-03-14 15:32:34 +0100

    fragmente erstellt

---
+++ app/src/main/java/com/example/heldenundboesewichtesyntaxintstitutaufgabe/ui/HereosFragment.kt
@@ -0,0 +1,20 @@
+package com.example.heldenundboesewichtesyntaxintstitutaufgabe.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.heldenundboesewichtesyntaxintstitutaufgabe.R
+
+class HereosFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_hereos, container, false)
+    }
+
+}
